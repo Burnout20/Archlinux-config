28402b4b86170d8175a79af0521823a6f2b7e303
Burnout20 <89000768+Burnout20@users.noreply.github.com>
2023-09-09 03:48:19 +0000

    Plugins Installed for c and python

---
+++ config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,5 @@
+---@type ChadrcConfig 
+ local M = {}
+ M.ui = {theme = 'chadracula'}
+ M.plugins = "custom.plugins"
+ return M
---
+++ config/nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,21 @@
+local base = require("plugins.configs.lspconfig")
+local on_attach = base.on_attach
+local capabilities = base.capabilities
+
+local lspconfig = require("lspconfig")
+
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetype = {"python"}
+})
+
+lspconfig.clangd.setup{
+  on_attach = function(client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufnr)
+  end,
+  capabilities = capabilities,
+}
+
+
