ca88ea07ad2869f60bc19e8e0ab413db931d1aa1
Burnout20 <89000768+Burnout20@users.noreply.github.com>
2023-09-21 10:37:26 +0000

    Unwanted c++ files ignore the Cmake ones

---
+++ projects/prac_c/Donut/donut2.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <locale>
+int main(){
+
+  float A = 0,B = 0;
+  float i, j;
+  int k;
+  float z[1760];
+  char b[1760];
+  printf("\x1b[2J");
+  for(;;) {
+    memset(b,32,1760);
+    memset(z,0,7040);
+    for(j=0; j < 6.28; j += 0.07){
+      for (i=0; i < 6.28; j += 0.02) {
+        float c = sin(i);
+        float d = cos(j);
+        float e = sin(A);
+        float f = sin(j);
+        float g = cos(A);
+
+
+      
+      }
+    }
+  }
+
+
+
+}
+
+
+
+
